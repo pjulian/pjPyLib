753525cd556532037927c99b7e72b6ea7d1cdb22
Philip Julian <pjulian@rackspace.com>
2025-01-08 10:30:30 -0600

    Test master branch protection.

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,14 +1,14 @@
 """pjPyLib utils unittests."""
 
-import uuid
-
 from pjpylib import utils
 
 
 def test_simple_uuid():
+    """Test simple_uuid."""
     _uuid = utils.simple_uuid()
     assert isinstance(_uuid, str)
 
 def test_make_uuid():
+    """Test make_uuid."""
     _uuid = utils.make_uuid(["test", "this"])
     assert isinstance(_uuid, str)
